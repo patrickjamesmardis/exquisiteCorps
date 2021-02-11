254ea53196ccc97527fba0e0ec4b9fbbb927e1b4
Patrick James Mardis <59069682+patrickjamesmardis@users.noreply.github.com>
2021-02-11 16:40:04 -0500

    add css and js comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll("section");
 
+// when an element is 30% in the viewport, add .in-view
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.intersectionRatio > 0.3 ? entry.target.classList.add("in-view") : entry.target.classList.remove("in-view");
@@ -8,6 +9,7 @@ const observer = new IntersectionObserver(entries => {
     threshold: [0.0, 0.3, 1.0]
 });
 
+// observe all sections to add .in-view on scroll
 sections.forEach(section => {
     observer.observe(section);
 });
